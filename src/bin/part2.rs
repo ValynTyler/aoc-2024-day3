ea1d8c452f23acc0de375f68eedb9ac6a8350d6f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 14:00:14 +0200

    feat: solved part 2

--- src/bin/part2.rs
+++ src/bin/part2.rs
@@ -1,22 +1,59 @@
 use regex::{Error, Regex};
 
+#[derive(Debug)]
+enum ProcessedInput {
+    Mul(i32, i32),
+    Ok,
+    No,
+}
+
 fn main() -> Result<(), Error> {
-    let puzzle_input = include_str!("../../input/example2.txt");
+    let puzzle_input = include_str!("../../input/instructions.txt");
 
-    let num_re = r".\d*";
-    let mul_re = format!(r"mul\({num_re},{num_re}\)");
+    let num = r".\d*";
+    let mul = format!(r"mul\({num},{num}\)");
 
-    let ok_re = r"do\(\)";
-    let no_re = r"don't\(\)";
+    let ok = r"do\(\)";
+    let no = r"don't\(\)";
 
-    let re = Regex::new(&format!(r"{mul_re}|{ok_re}|{no_re}"))?;
+    let re = Regex::new(&format!(r"{mul}|{ok}|{no}"))?;
 
-    let operands: Vec<_> = re
+    let processed_input: Vec<_> = re
         .captures_iter(puzzle_input)
         .map(|captures| {
-            println!("{:?}", captures);
+            let (sub, []) = captures.extract();
+            match sub {
+                "do()" => ProcessedInput::Ok,
+                "don't()" => ProcessedInput::No,
+                _s => {
+                    let mul_re = Regex::new(&format!(r"mul\(({num}),({num})\)")).unwrap();
+                    if let Some(cap) = mul_re.captures(sub) {
+                        let (_, [lhs, rhs]) = cap.extract();
+                        ProcessedInput::Mul(
+                            lhs.trim().parse().unwrap(), 
+                            rhs.trim().parse().unwrap(),
+                        )
+                    } else {
+                        panic!()
+                    }
+                }
+            }
         })
         .collect();
+
+    let mut sum = 0;
+    let mut flag = 1;
+    for item in processed_input {
+        match item {
+            ProcessedInput::Ok => flag = 1,
+            ProcessedInput::No => flag = 0,
+            ProcessedInput::Mul(lhs, rhs) => {
+                println!("{:?} {}", item, flag);
+                sum += lhs * rhs * flag;
+            }
+        }
+    }
+    println!("{sum}");
 
     Ok(())
 }
