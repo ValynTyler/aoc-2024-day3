10d7d537e3878c93451cb22508813175958ea16f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 11:27:51 +0200

    feat: solved part 1

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -1,12 +1,24 @@
 use regex::{Error, Regex};
 
 fn main() -> Result<(), Error> {
-    let puzzle_input = include_str!("../../input/example.txt");
-    let re = Regex::new(r"mul\(.\d*,.\d*\)")?;
+    let puzzle_input = include_str!("../../input/instructions.txt");
+    let re = Regex::new(r"mul\((.\d*),(.\d*)\)")?;
 
-    for item in re.find_iter(puzzle_input) {
-        println!("{}", item.as_str());
+    let operands: Vec<_> = re
+        .captures_iter(puzzle_input)
+        .map(|captures| {
+            let (_, [lhs, rhs]) = captures.extract();
+            let (lhs, rhs) = (lhs.parse::<i32>().unwrap(), rhs.parse::<i32>().unwrap());
+            (lhs, rhs)
+        })
+        .collect();
+
+    let mut sum = 0;
+    for item in operands {
+        println!("mul({}, {});", item.0, item.1);
+        sum += item.0 * item.1;
     }
+    println!("{}", sum);
 
     Ok(())
 }
