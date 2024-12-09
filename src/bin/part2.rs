38568b9585852fba5cbc79d50692308a04a80dec
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 13:39:56 +0200

    feat: now reading part 2 extra options

--- src/bin/part2.rs
+++ src/bin/part2.rs
@@ -1,2 +1,22 @@
-fn main() {
+use regex::{Error, Regex};
+
+fn main() -> Result<(), Error> {
+    let puzzle_input = include_str!("../../input/example2.txt");
+
+    let num_re = r".\d*";
+    let mul_re = format!(r"mul\({num_re},{num_re}\)");
+
+    let ok_re = r"do\(\)";
+    let no_re = r"don't\(\)";
+
+    let re = Regex::new(&format!(r"{mul_re}|{ok_re}|{no_re}"))?;
+
+    let operands: Vec<_> = re
+        .captures_iter(puzzle_input)
+        .map(|captures| {
+            println!("{:?}", captures);
+        })
+        .collect();
+
+    Ok(())
 }
