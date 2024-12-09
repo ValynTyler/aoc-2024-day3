072710de81e6ef9b9a6f3bbf52398626c57a793f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-09 11:10:07 +0200

    now picking out mul operations via regex

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -1,2 +1,12 @@
-fn main() {
+use regex::{Error, Regex};
+
+fn main() -> Result<(), Error> {
+    let puzzle_input = include_str!("../../input/example.txt");
+    let re = Regex::new(r"mul\(.\d*,.\d*\)")?;
+
+    for item in re.find_iter(puzzle_input) {
+        println!("{}", item.as_str());
+    }
+
+    Ok(())
 }
